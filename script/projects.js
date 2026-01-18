621b251d110538393ebfb722dd85357041b49830
Dio Lemos <67412764+Diolemos@users.noreply.github.com>
2026-01-18 17:46:55 -0300

    update pojects.js

--- script/projects.js
+++ script/projects.js
@@ -3,7 +3,7 @@ const projects = [
     title: "Food Order App",
     description:
       "A React front-end for a pastry shop ordering system, built with Hooks, Context API, and CSS Modules. Designed as a complete ordering flow and easily adaptable to other food businesses.",
-    image: "/imgs/food-app.png",
+    image: "imgs/food-app.png",
     live: "https://gostosuras-food-order-react-app.netlify.app/",
     code: "https://github.com/Diolemos/food-order-app"
   }
